b445d519b791aa05e775917cfe9354279846fca5
TobyKreiselmaier <the.rainmaker@mail.com>
2020-12-22 11:11:17 -0500

    Updated to solidity 0.8.0

--- Fundraiser.sol
+++ Fundraiser.sol
@@ -1,25 +1,38 @@
-pragma solidity ^0.4.8;
+/// SPDX-License-Identifier: UNLICENSED
 
+pragma solidity ^0.8.0;
+
+/// @title Fundraiser simulates DAO. Will be attacked by attacker.sol
 
 contract Fundraiser{
-    
+
+    /* Variables */
     mapping  (address => uint) balances;
-    
-    function contribute() payable{
+
+    /* Public functions */
+    /// @dev Collecting funds.
+    function contribute()
+        public payable
+    {
         balances[msg.sender] += msg.value;
     }
-    
-    function withdraw(){
+
+    /// @dev Withdraw funds.
+    function withdraw()
+        public
+    {
         if(balances[msg.sender] == 0){
-            throw;
+            revert;
         }
-        
         balances[msg.sender] = 0;
-        msg.sender.call.value(balances[msg.sender])();
+        msg.sender.call.value(balances[msg.sender]);
     }
-    
-    function getFunds() returns (uint){
+
+    /// @dev Get contract balance.
+    /// @return Returns contract balance.
+    function getFunds()
+        public returns (uint)
+    {
         return address(this).balance;
     }
-    
 }
