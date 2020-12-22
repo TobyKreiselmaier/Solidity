fb514b3b1ca2a35d42f6382ac34342faa29c2c81
TobyKreiselmaier <the.rainmaker@mail.com>
2020-12-22 13:05:06 -0500

    Updated to solidity 0.8.0

--- Testflipper.sol
+++ Testflipper.sol
@@ -1,29 +1,52 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
 import "./CoinFlipper.sol";
 
+/// @title Testfunctions for CoinFlipper.sol
+
 contract TestFlipper is CoinFlipper {
 
+    /* Test functions */
+    /// @dev Access _payout().
+    /// @param _amount Amount that is paid.
+    /// @param _to Recipient's address.
     function testPayout(uint256 amount, address payable to) public {
         _payout(amount, to);
     }
 
-    function testSetWaitingMapping(address user, bool status) public {
-            waitingPlayers[user] = status;
-    }
-
-    function testGetWaitingMapping(address user) public view returns (bool) {
+    /// @dev Access waitingPlayers[].
+    /// @param user Player to be checked if waiting on oracle.
+    function testGetWaitingMapping(address user)
+        public view returns (bool)
+    {
         return waitingPlayers[user];
     }
 
-    function testGetBetsMapping(bytes32 queryId) 
-            public view returns (address player, uint256 stake, uint256 choice) {
-        return (pendingBets[queryId].player, pendingBets[queryId].stake, pendingBets[queryId].choice);
+    /// @dev Access pendingBets[].
+    /// @param requestId Request Id from Chainlink.
+    /// @return Address of player, his stake, and initial choice with this request Id.
+    function testGetBetsMapping(bytes32 requestId)
+            public view returns (address player, uint256 stake, uint256 choice)
+    {
+        return (pendingBets[requestId].player, pendingBets[requestId].stake, pendingBets[requestId].choice);
     }
 
-    function testSetUserBalance(uint256 amount) public {//doesn't change contract balance
-        userBalances[msg.sender] = amount;//only for testing
+    /// @dev Set waitingPlayers[].
+    /// @param user Player that is to be set in the mapping.
+    /// @param status True = player is waiting | False = player is not waiting.
+    function testSetWaitingMapping(address user, bool status)
+        public 
+    {
+        waitingPlayers[user] = status;
+    }
+
+    /// @dev Set userBalances[].
+    /// @param amount Testamount to be set.
+    function testSetUserBalance(uint256 amount)
+        public 
+    {
+        userBalances[msg.sender] = amount;
     }
 }
--- Workers.sol
+++ Workers.sol
@@ -1,21 +1,40 @@
+/// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
 import "./People.sol";
-pragma solidity 0.5.12;
 
-contract Workers is People{ //- Should inherit from the People Contract. 
-   
-    mapping(address => uint) internal salary;//- Should extend the People contract by adding another mapping called salary which maps an address to an integer. 
-    mapping(address => address) internal manager;//For each worker created, you need to input and save information about who (which address) is the boss. This should be implemented in the Worker contract.
+/// @title Child of People contract.
 
-    function createWorker(address workerId, string memory name, uint age, uint height, uint wages) public{//- Have a createWorker function which is a wrapper function for the createPerson function. Make sure to figure out the correct visibility level for the createPerson function (it should no longer be public).
-        require(age <= 75, "Age can not exceed 75");//- When creating a worker, the persons age should not be allowed to be over 75. 
+contract Workers is People{
+
+    /* Variables */
+    mapping(address => uint) internal salary;
+    mapping(address => address) internal manager;
+
+    /* Public functions */
+    /// @dev Allows for creation of a new worker as a wrapper for createPerson()
+    /// @param workerId Unique id for workers.
+    /// @param name Name of worker.
+    /// @param age Age of worker.
+    /// @param height Height of worker.
+    /// @param wages Wages of worker.
+    function createWorker(address workerId, string memory name, uint age, uint height, uint wages)
+        public
+    {
+        require(age <= 75, "Age can not exceed 75");
         require(workerId != msg.sender, "Worker can not be his/her own boss");
-        createPerson(name, age, height);//- Apart from calling the createPerson function
-        salary[workerId] = wages;//the createWorker function should also set the salary for the Worker in the new mapping.
+        createPerson(name, age, height);
+        salary[workerId] = wages;
         manager[workerId] = msg.sender;
     }
-    
-    function fire(address workerId) public{
-        require(msg.sender == manager[workerId], "Caller needs to be the manager of this worker");//Make sure that a worker can only be fired by his/her boss.
+
+    /// @dev Allows for the removal of a worker
+    /// @param workerId Unique id for workers.
+    function fire(address workerId)
+        public
+    {
+        require(msg.sender == manager[workerId], "Caller needs to be the manager of this worker");
         deletePerson(workerId);
         delete salary[workerId];
         delete manager[workerId];
