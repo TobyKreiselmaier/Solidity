f651e064349246c2013bcc3af64e258abb38c836
TobyKreiselmaier <the.rainmaker@mail.com>
2020-12-22 11:18:44 -0500

    Updated to solidity 0.8.0

--- Library.sol
+++ Library.sol
@@ -1,35 +1,53 @@
-pragma solidity ^0.4.8;
+/// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+/// @title Library simulating Parity freeze attack.
 
 contract Library{
-    
+
+    /* Variables */
     address owner;
+
+    /* Modifiers */
+    modifier onlyOwner 
+    {
+        if(msg.sender != owner){
+            revert;
+        }
+        _;
+    }
     
-    function isNotPositive(uint number) returns (bool){
+
+    /* Public functions */
+    /// @dev Simple function that returns true/false.
+    /// @param number Number to be processed.
+    /// @return true - if number is negative | false - if number is positive.
+    function isNotPositive(uint number)
+        public returns (bool)
+    {
         if(number<=0){
             return true;
         }
         return false;
     }
-    
 
-    function destroy() public onlyOwner {
+    /// @dev Selfdestruct function
+    function destroy() 
+        public onlyOwner 
+    {
         selfdestruct(owner);
     }
     
-    modifier onlyOwner {
-        if(msg.sender != owner){
-            throw;
-        }
-        _;
-    }
-    
-    function initOwner(){
+    /// @dev Allows reassigning of owner. Here is where the contract is vulnerable.
+    function initOwner()
+        public 
+    {
         if(owner==address(0)){
             owner = msg.sender;
         }
         else{
-            throw;
+            revert;
         }
     }
-    
 }
