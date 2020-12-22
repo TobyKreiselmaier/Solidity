5d4ed65ff47e1fc1f63cf1d3be971fcdd76f501e
TobyKreiselmaier <the.rainmaker@mail.com>
2020-12-22 11:32:49 -0500

    Updated to solidity 0.8.0

--- Ownable.sol
+++ Ownable.sol
@@ -1,18 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
+
+/// @title Ownable allows to restrict functionality to deployer of contract.
 
 contract Ownable{
-   
+
+    /* Variable */
     address payable internal _owner;
-      
-    modifier onlyOwner(){
-      require(msg.sender == _owner, 
-      "You need to be owner of the contract in order to access this functionality!");
-      _;
+
+    /* Modifier */
+    modifier onlyOwner()
+    {
+        require(msg.sender == _owner, 
+        "You need to be owner of the contract in order to access this functionality!");
+        _;
     }
-    
-    constructor() public { 
-      _owner = msg.sender;
+
+    /* Constructor */
+    constructor()
+        public 
+    { 
+        _owner = msg.sender;
     }
-}      
+}
