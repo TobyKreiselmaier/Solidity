9da9502ac4905f24599234493eafb38cc9eeb5c6
TobyKreiselmaier <the.rainmaker@mail.com>
2020-12-22 12:49:03 -0500

    Updated to solidity 0.8.0

--- Proxy Coding/Storage.sol
+++ Proxy Coding/Storage.sol
@@ -1,13 +1,28 @@
-pragma solidity 0.5.1;
+/// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+/// @title Storage contract for proxy setup.
 
 contract Storage {
+
+    /* Variable */
     uint256 number;
 
-    function getNumber() internal view returns (uint){
+    /* Public functions */
+    /// @dev Getter for number of dogs.
+    /// @return number of dogs.
+    function getNumber()
+        internal view returns (uint)
+    {
         return number;
     }
 
-    function setNumber(uint256 _number) internal{
+    /// @dev Setter for number of dogs.
+    /// @param _number Number of dogs.
+    function setNumber(uint256 _number)
+        internal
+    {
         number = _number;
     }
 }
